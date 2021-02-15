0c468d8229249dfa28933f0501f1e800111957ae
br-d <ropejko.dev@gmail.com>
2021-02-15 11:26:25 +0100

    Prepare RegisterActivity.kt for handling registration. Prepare SetupActivity.kt for new user setup.

--- app/src/main/java/com/example/fspace/RegisterActivity.kt
+++ app/src/main/java/com/example/fspace/RegisterActivity.kt
@@ -2,10 +2,25 @@ package com.example.fspace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class RegisterActivity : AppCompatActivity() {
+
+    lateinit var emailEditText: EditText
+    lateinit var passwordEditText: EditText
+    lateinit var confirmPasswordEditText: EditText
+    lateinit var registerButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        emailEditText = findViewById<EditText>(R.id.registerEmailEditText)
+        passwordEditText = findViewById<EditText>(R.id.registerPasswordEditText)
+        confirmPasswordEditText = findViewById<EditText>(R.id.registerConfirmPasswordEditText)
+        registerButton = findViewById<Button>(R.id.registerConfirmationButton)
+
+
     }
 }
--- app/src/main/java/com/example/fspace/SetupActivity.kt
+++ app/src/main/java/com/example/fspace/SetupActivity.kt
@@ -2,10 +2,28 @@ package com.example.fspace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import de.hdodenhof.circleimageview.CircleImageView
 
 class SetupActivity : AppCompatActivity() {
+
+    lateinit var fullnameEditText: EditText
+    lateinit var usernameEditText: EditText
+    lateinit var countryEditText: EditText
+    lateinit var confirmButton: Button
+    lateinit var profileCircleImageView: CircleImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setup)
+
+        fullnameEditText = findViewById<EditText>(R.id.setupFullname)
+        usernameEditText = findViewById<EditText>(R.id.setupUsername)
+        countryEditText = findViewById<EditText>(R.id.setupCountry)
+        confirmButton = findViewById<Button>(R.id.setupConfirmButton)
+        profileCircleImageView = findViewById<CircleImageView>(R.id.setupProfilePicture)
+
+
     }
 }
