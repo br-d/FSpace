36313d2fb9be027dc4a82df984e4790bb43fda98
br-d <ropejko.dev@gmail.com>
2021-02-15 10:44:06 +0100

    Add check if user is authenticated or send to LoginActivity.kt. Add redirection to RegisterActivity.kt when clicked the redirect link on activity_login.xml

--- app/src/main/java/com/example/fspace/LoginActivity.kt
+++ app/src/main/java/com/example/fspace/LoginActivity.kt
@@ -1,11 +1,37 @@
 package com.example.fspace
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var loginButton: Button
+    lateinit var emailEditText: EditText
+    lateinit var passwordEditText: EditText
+    lateinit var redirectToRegisterTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        redirectToRegisterTextView = findViewById<TextView>(R.id.loginLinkToRegistration)
+        emailEditText = findViewById<EditText>(R.id.loginEmailEditText)
+        passwordEditText = findViewById<EditText>(R.id.loginPasswordEditText)
+        loginButton = findViewById<Button>(R.id.loginButton)
+
+        redirectToRegisterTextView.setOnClickListener{
+            sendUserToRegisterActivity()
+        }
     }
+
+    private fun sendUserToRegisterActivity() {
+        var registerIntent = Intent(this@LoginActivity, RegisterActivity::class.java)
+        startActivity(registerIntent)
+        finish()
+    }
+
 }
